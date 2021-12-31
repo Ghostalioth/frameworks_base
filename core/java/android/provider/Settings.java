56adb63fefdb1812de5b40b2b13eeff022b9fd35
Dil3mm4 <dil3mm4.dev@gmail.com>
2021-12-31 11:21:49 +0800

    SystemUI: Implement background process killer [1/2]
    * rebranded/adapted from Descendant-XI/frameworks_base@4d6a3fa
    * nuked stuff we dont want to mess with like sync and location
    
    Change-Id: I77ad8dedddb19403f54926183edef4c3406185b9
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6392,6 +6392,13 @@ public final class Settings {
                 "notification_guts_kill_app_button";
 
         /**
+         * Arcane Idle Manager
+         * @hide
+         */
+        @Readable
+        public static final String CUSTOM_IDLE_MANAGER = "custom_idle_manager";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
