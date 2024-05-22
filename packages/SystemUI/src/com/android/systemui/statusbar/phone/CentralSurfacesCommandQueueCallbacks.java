e438fc263efcdbf1279beb8c5affc540aa527e71
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-22 18:07:21 +0800

    SystemUI: Use VibrationUtils for QS/Status bar haptics
    * VibrationsUtils uses single thread executor compared to
      vibratorhelper's performHapticFeedback,single thread seems to play nicer
      with animations compared to async/normal vibration callbacks based from our tests.
      We initially observed this when upstreaming VibrationUtils coming from async tasks
      where we observed hiccups/lags when triggering vibrations
      for edge back gesture animation.
    
    test: swipe QS panel, no hiccups observed when expanding QS with media player
    which is better compared to performHapticFeedback where we observed a bit
    of hiccups especially when qs media player is visible
    
    Change-Id: I7f28307e75fbc47ac5e17927b1a6cadeb1b1f677
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesCommandQueueCallbacks.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesCommandQueueCallbacks.java
@@ -70,6 +70,8 @@ import com.android.systemui.statusbar.policy.HeadsUpManager;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.RemoteInputQuickSettingsDisabler;
 
+import com.android.internal.util.tenx.VibrationUtils;
+
 import dagger.Lazy;
 
 import java.util.Arrays;
@@ -321,7 +323,7 @@ public class CentralSurfacesCommandQueueCallbacks implements CommandQueue.Callba
             mMetricsLogger.action(MetricsEvent.ACTION_SYSTEM_NAVIGATION_KEY_DOWN);
             if (mPanelExpansionInteractor.isFullyCollapsed()) {
                 if (mVibrateOnOpening) {
-                    vibrateOnNavigationKeyDown();
+                    VibrationUtils.triggerVibration(mContext, 2);
                 }
                 mShadeController.animateExpandShade();
                 mNotificationStackScrollLayoutController.setWillExpand(true);
