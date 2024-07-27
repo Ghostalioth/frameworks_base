fd87b75e012888d6a30136d3fc6bb4459cc26d0b
minaripenguin <minaripenguin@users.noreply.github.com>
2024-07-27 11:30:52 +0800

    SystemUI: Improve brightness slider haptics
    Change-Id: Ia4be569a28de48209693a024b99d5257cfedf131
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -114,6 +114,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
     private float mBrightnessMax = PowerManager.BRIGHTNESS_MAX;
 
     private ValueAnimator mSliderAnimator;
+    private long lastVibrateTime = 0;
 
     private Vibrator mVibrator;
     private static final VibrationEffect BRIGHTNESS_SLIDER_HAPTIC =
@@ -376,8 +377,13 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
         setBrightness(valFloat);
 
         // Give haptic feedback only if brightness is changed manually
-        if (mBrightnessSliderHaptic > 0 && mVibrator != null && tracking)
-            VibrationUtils.triggerVibration(mContext, mBrightnessSliderHaptic);
+        if (mBrightnessSliderHaptic > 0 && mVibrator != null && tracking) {
+            long currentTime = System.currentTimeMillis();
+            if (currentTime - lastVibrateTime >= 100L) {
+                VibrationUtils.triggerVibration(mContext, mBrightnessSliderHaptic);
+                lastVibrateTime = currentTime;
+            }
+        }
 
         if (!tracking) {
             AsyncTask.execute(new Runnable() {
