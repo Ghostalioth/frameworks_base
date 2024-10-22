25e5acc9247785e938c9bd322f97373ab0360ca0
Ghosuto <clash.raja10@gmail.com>
2024-10-22 17:31:39 +0000

    base: Improve systemutils
    Change-Id: I312b0dff484d93da3ea35114a503008b15b48e4c

--- core/java/com/android/internal/util/custom/systemUtils.java
+++ core/java/com/android/internal/util/custom/systemUtils.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2017-2022 crDroid Android Project
- * Copyright (C) 2023 Rising OS Android Project
+ * Copyright (C) 2024 TenX-OS Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,86 +19,29 @@ package com.android.internal.util.custom;
 import android.app.AlertDialog;
 import android.app.IActivityManager;
 import android.app.ActivityManager;
-import android.content.DialogInterface;
 import android.content.Context;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Handler;
-import android.os.PowerManager;
 import android.os.RemoteException;
-import android.os.SystemClock;
 import android.os.ServiceManager;
-import android.os.SystemProperties;
 
 import com.android.internal.R;
 import com.android.internal.statusbar.IStatusBarService;
 
 import java.lang.ref.WeakReference;
-import java.util.List;
 
 public class systemUtils {
 
-    public static boolean isPackageInstalled(Context context, String packageName, boolean ignoreState) {
-        if (packageName != null) {
-            try {
-                PackageInfo pi = context.getPackageManager().getPackageInfo(packageName, 0);
-                if (!pi.applicationInfo.enabled && !ignoreState) {
-                    return false;
-                }
-            } catch (PackageManager.NameNotFoundException e) {
-                return false;
-            }
-        }
-        return true;
-    }
+    private static final int RESTART_TIMEOUT = 1000;
 
-    public static boolean isPackageInstalled(Context context, String packageName) {
-        return isPackageInstalled(context, packageName, true);
-    }
-
-    public static boolean isPackageEnabled(Context context, String packageName) {
-        try {
-            PackageInfo pi = context.getPackageManager().getPackageInfo(packageName, 0);
-            return pi.applicationInfo.enabled;
-        } catch (PackageManager.NameNotFoundException notFound) {
-            return false;
-        }
-    }
-
-    public static void switchScreenOff(Context ctx) {
-        PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
-        if (pm!= null) {
-            pm.goToSleep(SystemClock.uptimeMillis());
-        }
-    }
-
-    public static boolean deviceHasFlashlight(Context ctx) {
-        return ctx.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
-    }
-
-    public static boolean hasNavbarByDefault(Context context) {
-        boolean needsNav = context.getResources().getBoolean(
-                com.android.internal.R.bool.config_showNavigationBar);
-        String navBarOverride = SystemProperties.get("qemu.hw.mainkeys");
-        if ("1".equals(navBarOverride)) {
-            needsNav = false;
-        } else if ("0".equals(navBarOverride)) {
-            needsNav = true;
-        }
-        return needsNav;
-    }
-    
-   public static void showSystemRestartDialog(Context context) {
+    public static void showSystemRestartDialog(Context context) {
         new AlertDialog.Builder(context)
                 .setTitle(R.string.system_restart_title)
                 .setMessage(R.string.system_restart_message)
-                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Handler handler = new Handler();
-                        handler.postDelayed(() -> restartSystem(context), 2000);
-                    }
+                .setPositiveButton(R.string.ok, (dialog, id) -> {
+                    Handler handler = new Handler();
+                    handler.postDelayed(() -> restartSystem(context), RESTART_TIMEOUT);
                 })
                 .setNegativeButton(R.string.cancel, null)
                 .show();
@@ -112,8 +54,7 @@ public class systemUtils {
     private static class RestartSystemTask extends AsyncTask<Void, Void, Void> {
         private final WeakReference<Context> mContext;
 
-        public RestartSystemTask(Context context) {
-            super();
+        RestartSystemTask(Context context) {
             mContext = new WeakReference<>(context);
         }
 
@@ -124,8 +65,8 @@ public class systemUtils {
                         ServiceManager.getService(Context.STATUS_BAR_SERVICE));
                 if (mBarService != null) {
                     try {
-                        Thread.sleep(1250);
-                        mBarService.reboot(false);
+                        Thread.sleep(RESTART_TIMEOUT);
+                        mBarService.reboot(false, null);
                     } catch (RemoteException | InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -137,30 +78,29 @@ public class systemUtils {
         }
     }
 
-    public static void showSettingsRestartDialog(Context context) {
+    private static void showRestartDialog(Context context, int title, int message, Runnable action) {
         new AlertDialog.Builder(context)
-                .setTitle(R.string.settings_restart_title)
-                .setMessage(R.string.settings_restart_message)
-                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Handler handler = new Handler();
-                        handler.postDelayed(() -> restartSettings(context), 2000);
-                    }
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(R.string.ok, (dialog, id) -> {
+                    Handler handler = new Handler();
+                    handler.postDelayed(action, RESTART_TIMEOUT);
                 })
                 .setNegativeButton(R.string.cancel, null)
                 .show();
     }
 
-    public static void restartSettings(Context context) {
-        new RestartSettingsTask(context).execute();
+    public static void restartProcess(Context context, String processName) {
+        new RestartTask(context, processName).execute();
     }
 
-    private static class RestartSettingsTask extends AsyncTask<Void, Void, Void> {
+    private static class RestartTask extends AsyncTask<Void, Void, Void> {
         private final WeakReference<Context> mContext;
+        private final String mProcessName;
 
-        public RestartSettingsTask(Context context) {
-            super();
+        RestartTask(Context context, String processName) {
             mContext = new WeakReference<>(context);
+            mProcessName = processName;
         }
 
         @Override
@@ -170,7 +110,7 @@ public class systemUtils {
                 if (am != null) {
                     IActivityManager ams = ActivityManager.getService();
                     for (ActivityManager.RunningAppProcessInfo app : am.getRunningAppProcesses()) {
-                        if ("com.android.settings".equals(app.processName)) {
+                        if (app.processName.contains(mProcessName)) {
                             ams.killApplicationProcess(app.processName, app.uid);
                             break;
                         }
@@ -181,52 +121,13 @@ public class systemUtils {
             }
             return null;
         }
+    }
+
+    public static void showSettingsRestartDialog(Context context) {
+        showRestartDialog(context, R.string.settings_restart_title, R.string.settings_restart_message, () -> restartProcess(context, "com.android.settings"));
     }
 
     public static void showSystemUIRestartDialog(Context context) {
-        new AlertDialog.Builder(context)
-                .setTitle(R.string.systemui_restart_title)
-                .setMessage(R.string.systemui_restart_message)
-                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Handler handler = new Handler();
-                        handler.postDelayed(() -> restartSystemUI(context), 2000);
-                    }
-                })
-                .setNegativeButton(R.string.cancel, null)
-                .show();
+        showRestartDialog(context, R.string.systemui_restart_title, R.string.systemui_restart_message, () -> restartProcess(context, "com.android.systemui"));
     }
-
-    public static void restartSystemUI(Context context) {
-        new RestartSystemUITask(context).execute();
-    }
-
-    private static class RestartSystemUITask extends AsyncTask<Void, Void, Void> {
-        private final WeakReference<Context> mContext;
-
-        public RestartSystemUITask(Context context) {
-            super();
-            mContext = new WeakReference<>(context);
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                ActivityManager am = (ActivityManager) mContext.get().getSystemService(Context.ACTIVITY_SERVICE);
-                if (am != null) {
-                    IActivityManager ams = ActivityManager.getService();
-                    for (ActivityManager.RunningAppProcessInfo app : am.getRunningAppProcesses()) {
-                        if ("com.android.systemui".equals(app.processName)) {
-                            ams.killApplicationProcess(app.processName, app.uid);
-                            break;
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-    }
-
 }
