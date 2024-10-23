19e13c8a35d02c231d55af3cc2a06bdb02720c63
Michael Bestas <mkbestas@lineageos.org>
2024-10-23 19:11:50 +0800

    fixup! SystemUI: Status bar tuner improvements
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
@@ -28,6 +28,10 @@ import java.util.ArrayDeque;
 
 import java.util.ArrayDeque;
 
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.graphics.Insets;
+
 import java.util.ArrayDeque;
 
 import androidx.preference.Preference;
@@ -39,6 +43,8 @@ import com.android.systemui.demomode.DemoModeController;
 import com.android.systemui.fragments.FragmentService;
 import com.android.systemui.res.R;
 import com.android.systemui.util.settings.GlobalSettings;
+
+import com.google.android.material.color.DynamicColors;
 
 import javax.inject.Inject;
 
@@ -73,6 +79,22 @@ public class TunerActivity extends CollapsingToolbarBaseActivity implements
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         setContentView(R.layout.tuner_activity);
+
+        DynamicColors.applyToActivityIfAvailable(this);
+        setTheme(com.android.settingslib.widget.theme.R.style.Theme_SubSettingsBase);
+
+        // Handle window insets for padding adjustments
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.content_frame), (view, insets) -> {
+            Insets systemInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+
+            view.setPadding(
+                view.getPaddingLeft(),
+                view.getPaddingTop(),
+                view.getPaddingRight(),
+                systemInsets.bottom
+            );
+            return insets;
+        });
 
         if (getFragmentManager().findFragmentByTag(TAG_TUNER) == null) {
             final String action = getIntent().getAction();
